822b0d5b014aae702207c98d45928be954458bfa
MAC <mac@MACs-MacBook-Pro.local>
2023-02-24 15:06:56 -0500

    fetchData 1

--- screens/HomeNews.js
+++ screens/HomeNews.js
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React, { useEffect, useState } from "react";
 import tw from "twrnc";
 import { Ionicons } from "@expo/vector-icons";
@@ -11,11 +11,12 @@ import Card from "../components/Card";
 import { useGetTrendingNewsQuery } from "../services/trendingNewsApi";
 
 const HomeNews = () => {
+  const demoImage = "https://picsum.photos";
   const { data: trendingNews, isFetching } = useGetTrendingNewsQuery();
-  //   console.log(trendingNews);
+  // console.log(trendingNews);
 
-  const dataItem = trendingNews?.value?.image;
-  console.log(dataItem);
+  const dataItem = trendingNews?.value;
+  // console.log(dataItem);
 
   if (isFetching) return <Text>Loading</Text>;
 
@@ -28,34 +29,40 @@ const HomeNews = () => {
   //   }
 
   return (
-    <View style={tw`pt-15 pl-2 pr-2 `}>
-      {/* <Text>{trendingNews?.value?.NewsArticle} hmm</Text> */}
+    <ScrollView>
+      <View style={tw`pt-15 pl-2 pr-2 `}>
+        {/* <Text>{trendingNews?.value?.data?.NewsArticle} hmm</Text> */}
 
-      <View
-        style={[
-          tw`content-center items-center`,
-          { display: "flex", flexDirection: "row" },
-        ]}
-      >
-        <Text style={[tw`pr-1`]}>
-          <Ionicons name="newspaper" size={30} color="black" />
+        <View
+          style={[
+            tw`content-center items-center`,
+            { display: "flex", flexDirection: "row" },
+          ]}
+        >
+          <Text style={[tw`pr-1`]}>
+            <Ionicons name="newspaper" size={30} color="black" />
+          </Text>
+
+          <Text style={tw`text-2xl font-bold`}>News</Text>
+        </View>
+        <Text style={[tw`text-2xl font-bold `, { color: "gray" }]}>
+          {/* {CurrentDate()} */}
+          Today
         </Text>
+        <Divider />
 
-        <Text style={tw`text-2xl font-bold`}>News</Text>
+        {trendingNews?.value.map((news, i) => (
+          <Card
+            key={i}
+            // image="https://awildgeographer.files.wordpress.com/2015/02/john_muir_glacier.jpg"
+            image={news?.thumbnail || demoImage}
+            name={news.name}
+            title="The title"
+            time={news.datePublished}
+          />
+        ))}
       </View>
-      <Text style={[tw`text-2xl font-bold `, { color: "gray" }]}>
-        {/* {CurrentDate()} */}
-        Today
-      </Text>
-      <Divider />
-
-      <Card
-        image="https://awildgeographer.files.wordpress.com/2015/02/john_muir_glacier.jpg"
-        name="the name"
-        title="The title"
-        time="current time"
-      />
-    </View>
+    </ScrollView>
   );
 };
 
